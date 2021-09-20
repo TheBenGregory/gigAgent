317d6c4b1bb4f0f6361a68fcfd50f3082cc06477
Ben Gregory <JBGregoryPro@protonmail.com>
2021-09-20 15:50:25 -0500

    available agents are now clickable links that lead to TicketForm

--- src/Components/apiManager/apiManager.js
+++ src/Components/apiManager/apiManager.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react"
 
 export const getUsers = () => {
-   
-       return fetch("http://localhost:8088/user")
-            .then(res => res.json()) }
+
+     return fetch("http://localhost:8088/user")
+          .then(res => res.json())
+}
 
 
 export const getCurrentUser = () => {
-          const user = localStorage.getItem("agent_user")
-       return fetch("http://localhost:8088/user")
-            .then(res => res.json()) }
+     const user = localStorage.getItem("agent_user")
+     return fetch("http://localhost:8088/user")
+          .then(res => res.json())
+}
 
-
+// export const setReqJob = () => {
+//      const user = localStorage.getItem("agent_user")
+//      fetch("http://localhost:8088/reqJob", {
+//           method: "POST",
+//           headers: {
+//               "Content-Type": "application/json"
+//           },
+//           body: JSON.stringify(reqJob)
 // export const deleteUser = () => {
 //      const user = localStorage.getItem("agent_user")
 //      return fetch("http://localhost:8088/users",
