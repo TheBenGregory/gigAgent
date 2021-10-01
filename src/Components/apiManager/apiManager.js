6f3e4ad24557c97231b1ca3909cb49365549174a
Ben Gregory <JBGregoryPro@protonmail.com>
2021-10-01 12:12:09 -0500

    pic uploader works to JSON

--- src/Components/apiManager/apiManager.js
+++ src/Components/apiManager/apiManager.js
@@ -12,3 +12,20 @@ export const getCurrentUser = () => {
 }
 
 
+// export const setPictures = (id, img) => {
+//      return fetch(`http://localhost:8088/users/${id}`, {
+//          method: "PATCH",
+//          body: JSON.stringify({
+//              imageURL: img
+//          }),
+//          headers: {
+//              "Content-Type": "application/json"
+//          },
+//      })
+      
+//  }
+
+//  export const getPictures = () => {
+//      return fetch("http://localhost:https://api.cloudinary.com/v1_1/dsmjpyt8q/image/upload/users")
+//           .then(res => res.json())
+// }
