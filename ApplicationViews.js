699c8af2f22b429f260ce4c7614f3cc1019de77b
Ben Gregory <JBGregoryPro@protonmail.com>
2021-11-17 08:48:34 -0600

    redeploy test

--- ApplicationViews.js
+++ ApplicationViews.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { HomePage } from "./HomePage/Homepage";
-import { About } from "./About/about.js"
-import { TicketForm } from "./Tickets/Ticketform";
-import { Providers } from "./Providers/providers";
-import { Profile } from "./UserProfile/userprofile";
-import { TicketList } from "./Tickets/TicketList";
+import { HomePage } from "./src/Components/HomePage/Homepage";
+import { About } from "./src/Components/About/about.js"
+import { TicketForm } from "./src/Components/Tickets/Ticketform";
+import { Providers } from "./src/Components/Providers/providers";
+import { Profile } from "./src/Components/UserProfile/userprofile";
+import { TicketList } from "./src/Components/Tickets/TicketList";
 
 
 export const ApplicationViews = () => {
