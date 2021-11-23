bc48048f221a042fbd0de404d766fb7cb2c99a22
Ben Gregory <JBGregoryPro@protonmail.com>
2021-11-23 08:51:06 -0600

    store fix

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
-import { HomePage } from "./src/Components/HomePage/Homepage";
-import { About } from "./src/Components/About/about.js"
-import { TicketForm } from "./src/Components/Tickets/Ticketform";
-import { Providers } from "./src/Components/Providers/providers";
-import { Profile } from "./src/Components/UserProfile/userprofile";
-import { TicketList } from "./src/Components/Tickets/TicketList";
+import { HomePage } from "src/Components/HomePage/Homepage.js";
+import { About } from "src/Components/About/about.js"
+import { TicketForm } from "/src/Components/Tickets/Ticketform";
+import { Providers } from "src/Components/Providers/providers.js";
+import { Profile } from "/src/Components/UserProfile/userprofile.js";
+import { TicketList } from "/src/Components/Tickets/TicketList";
 
 
 export const ApplicationViews = () => {
